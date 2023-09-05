3a7e84a86fe9ea0791062a7edf133e5966690bee
dattc <dattc.for.work@gmail.com>
2023-09-05 10:54:01 +0700

    end

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello ${process.env.NAMEE} ${os.hostname()}</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello ${process.env.NAMEE} today is ${process.env.NOW} ${os.hostname()}</h1></body>`
   );
 });
 
